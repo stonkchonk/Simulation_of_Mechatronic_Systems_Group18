b365a04881379c7cc6f6554bc2d9354f06999a24
stonkchonk <heilfred98@gmail.com>
2025-12-16 23:22:37 +0100

    can extract values from simulink, optimizer wip

--- Exercise3/ex3c.m
+++ Exercise3/ex3c.m
@@ -2,14 +2,20 @@
 par3.m1 = 100;          % mass 1
 par3.m2 = 10;           % mass 2
 par3.C1 = 15;           % Spring value
-par3.C2 = 2;            % Spring value
-par3.D2 = 5;            % damper value
+par3.C2 = 1;            % Spring value
+par3.D2 = 1;            % damper value
 par3.t.start = 0;       % start simulation time
 par3.t.stop = 1000;       % stop simulation time
 
 
 out3c = sim('ex3c_simulink.slx');
-
-plot(out3c.getElement('x1').Values);
+time = out3c.x1x2.time;
+x1 = out3c.x1x2.signals(1).values;
+x2 = out3c.x1x2.signals(2).values;
+out_squared = x1.^2;
+area = trapz(time, out_squared);
+disp('the calculated area: ');
+disp(area);
+plot(time, x1, time, x2, time, out_squared);
 xlabel('time [sec]');
 grid;
