916ce930f472d1dfa318b10582a249fb81acbca6
stonkchonk <heilfred98@gmail.com>
2026-01-08 15:47:43 +0100

    simulink model for dcmotor, wip

---
+++ Exercise4/model_runner.m
@@ -0,0 +1,5 @@
+clc;clear;clf;
+
+par = param_setter(0.23*10^(-3), 23.4*10^(-3), 2.4, 0.23*10^(-6), 0.4191*10^(-5));
+
+out1 = sim('ex4_dcmotor'); 
---
+++ Exercise4/param_setter.m
@@ -0,0 +1,7 @@
+function [par] = param_setter(l, cm, r, j, d)
+    par.l = l;      % [H]
+    par.cm = cm;    % [Nm/A]
+    par.r = r;      % [Ohm]
+    par.j = j;      % [Nm]
+    par.d = d;
+end
